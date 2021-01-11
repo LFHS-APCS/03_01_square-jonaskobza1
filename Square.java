9bc80ea0439b4035dcaef50496722ef261e93672
Jonas Kobza <2320006@lfschools.net>
2021-01-11 21:11:12 +0000

    Done

--- Square.java
+++ Square.java
@@ -1,1 +1,31 @@
 
+public class Square{
+
+
+private double side;
+public final int MAX_SIDE_LENGTH = 10;
+
+
+
+public Square(double aSide){
+  side = aSide;
+}
+public void setSide(double theSide){
+  if (theSide <= MAX_SIDE_LENGTH && theSide > 0){
+    side = theSide;
+  }
+}
+public double getSide(){
+  return side;
+}
+
+public String toString(){
+  return "Square with side length = " +side;
+}
+public double area(){
+  return side * side;
+}
+public double diagonal(){
+  return side * 1.414213562;
+}
+}
